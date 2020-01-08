742d6cf331403db664c13faa9bc2a4fb5353cc27
Edward Tang <edward.tang@shopify.com>
2020-01-08 16:38:30 -0500

    Create associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  has_many :comments
+  has_many :tags, through: :tagged_posts
+  belongs_to :category
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
