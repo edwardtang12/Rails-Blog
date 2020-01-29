e704133f2c069dac996ec3e69a6d1ed05e372011
Edward Tang <edward.tang@shopify.com>
2020-01-29 13:42:20 -0500

    Add skeleton CSS

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def truncateString(string)
+    if string.length > 250
+      "#{string[0..249]} ..."
+    else
+      string
+    end
+  end
+
+  def returnDate(post)
+    post.created_at.strftime("%B %-d, %Y")
+  end
 end
